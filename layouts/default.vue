12775ec9b305d77710b0336328157d5f523ae984
艾伦耶格尔 <hanqiao1024@163.com>
2024-08-14 22:25:32 +0800

    feat: add default temp

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,12 +5,56 @@
 
 <template>
   <Html class="dark">
-    <slot />
+    <header>
+      <Favor />
+      <ThemeSwitch />
+    </header>
+
+    <div class="nuxt-content">
+      <slot />
+    </div>
+
   </Html>
 </template>
 
 <style lang="scss">
-p {
-  margin: 0;
+#__nuxt {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+
+  .nuxt-content {
+    flex: 1;
+  }
+}
+
+header {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 2px 20px;
+}
+
+/** Animated Theme Toggle */
+::view-transition-old(root),
+::view-transition-new(root) {
+  animation: none;
+  mix-blend-mode: normal;
+}
+
+.dark::view-transition-old(root) {
+  z-index: 1;
+}
+
+.dark::view-transition-new(root) {
+  z-index: 999;
+}
+
+::view-transition-old(root) {
+  z-index: 999;
+}
+
+::view-transition-new(root) {
+  z-index: 1;
 }
 </style>
