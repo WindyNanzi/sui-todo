e1bf1f1073167d32d4bb7be19dc8795cd9bfb632
艾伦耶格尔 <hanqiao1024@163.com>
2024-08-23 20:50:59 +0800

    chore: 重新发布

--- composables/config.ts
+++ composables/config.ts
@@ -7,7 +7,7 @@ export const SUI_CLIENT = computed(() => unref(useClient(unref(SUI_CURRENT_NODE_
 export const PACKAGE_ID = computed(() => {
   const env = unref(SUI_CURRENT_ENV)
   if(env === 'dev') {
-    return '0xdeb1b826e54ee28fe5513884e59ba2c37dc1a2e93cde2fc19fee656f4710cc57'
+    return '0x07df68b720fcb2070ced97096dc59b7f5d8b0b52ca16cb2e3fdb67bec48a841f'
   } else if(env === 'test') {
     return '0x4c4bbf49cb8924406e5b8f80c96a15ff4d8308785f8a5f2e2696fbdd5b3d2ce9'
   }
